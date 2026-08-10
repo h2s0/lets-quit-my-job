89f80faa081f177ca6c1b3f251fc6141572b7800
김희소 <h2s0_@naver.com>
2026-08-10 21:15:06 +0900

    feat: 컨페티 폭죽 모션을 캔버스로 구현

---
+++ src/components/ConfettiBurst.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from 'react';
+import confetti from 'canvas-confetti';
+import './ConfettiBurst.css';
+
+const BURST_INTERVAL = 2400;
+
+export default function ConfettiBurst() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const burst = confetti.create(canvasRef.current, {
+      resize: true,
+      useWorker: true,
+    });
+
+    const fire = () => {
+      const common = {
+        angle: 90,
+        spread: 360,
+        origin: { x: 0.5, y: 0.45 },
+        colors: ['#f44336', '#ff7043', '#087cff', '#29a8ff', '#ffd21f'],
+        decay: 0.91,
+        gravity: 0.14,
+        drift: 0,
+        disableForReducedMotion: true,
+      };
+
+      burst({
+        ...common,
+        particleCount: 72,
+        startVelocity: 43,
+        scalar: 0.82,
+        ticks: 155,
+      });
+      burst({
+        ...common,
+        particleCount: 32,
+        startVelocity: 27,
+        scalar: 0.58,
+        ticks: 135,
+      });
+    };
+
+    fire();
+    const timer = window.setInterval(fire, BURST_INTERVAL);
+
+    return () => {
+      window.clearInterval(timer);
+      burst.reset();
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} className="confetti-canvas" aria-hidden="true" />;
+}
--- src/components/MoneyRain.tsx
+++ src/components/MoneyRain.tsx
@@ -1,4 +1,5 @@
 import type { CSSProperties } from 'react';
+import ConfettiBurst from './ConfettiBurst';
 import './MoneyRain.css';
 
 const notePaths = [
@@ -22,31 +23,11 @@ const notePaths = [
   [9, '165px', '170px', '320px', '60px', '112px', '187px', '352px', '28deg'],
 ] as const;
 
-const confettiPaths = [
-  ['130px', '-320px', '46px', '-112px', '156px', '-384px', '110deg'],
-  ['-150px', '-270px', '-53px', '-95px', '-180px', '-324px', '-130deg'],
-  ['185px', '-40px', '65px', '-14px', '222px', '-48px', '160deg'],
-  ['-185px', '110px', '-65px', '39px', '-222px', '132px', '-150deg'],
-  ['110px', '300px', '39px', '105px', '132px', '360px', '135deg'],
-  ['-120px', '330px', '-42px', '116px', '-144px', '396px', '-120deg'],
-  ['190px', '180px', '67px', '63px', '228px', '216px', '145deg'],
-  ['-185px', '-150px', '-65px', '-53px', '-222px', '-180px', '-140deg'],
-  ['40px', '-360px', '14px', '-126px', '48px', '-432px', '170deg'],
-  ['-65px', '-350px', '-23px', '-123px', '-78px', '-420px', '-165deg'],
-  ['155px', '-225px', '54px', '-79px', '186px', '-270px', '125deg'],
-  ['-195px', '-25px', '-68px', '-9px', '-234px', '-30px', '-155deg'],
-  ['195px', '35px', '68px', '12px', '234px', '42px', '155deg'],
-  ['-165px', '250px', '-58px', '88px', '-198px', '300px', '-125deg'],
-  ['145px', '285px', '51px', '100px', '174px', '342px', '130deg'],
-  ['-55px', '365px', '-19px', '128px', '-66px', '438px', '-170deg'],
-  ['65px', '355px', '23px', '124px', '78px', '426px', '165deg'],
-  ['0px', '-380px', '0px', '-133px', '0px', '-456px', '180deg'],
-] as const;
-
 export default function MoneyRain() {
   return (
     <div className="money-rain" aria-hidden="true">
       <img className="money-burst" src="/severance-burst.png" alt="" />
+      <ConfettiBurst />
       {notePaths.map(([asset, size, endX, endY, kickX, kickY, farX, farY, rotation], index) => (
         <img
           className="money-note"
@@ -62,23 +43,6 @@ export default function MoneyRain() {
             '--far-y': farY,
             '--note-rotation': rotation,
             '--note-size': size,
-          } as CSSProperties}
-        />
-      ))}
-      {confettiPaths.map(([endX, endY, kickX, kickY, farX, farY, rotation], index) => (
-        <img
-          className="confetti-piece"
-          key={`confetti-${index}`}
-          src={`/confetti-piece-${(index % 5) + 1}.png`}
-          alt=""
-          style={{
-            '--end-x': endX,
-            '--end-y': endY,
-            '--kick-x': kickX,
-            '--kick-y': kickY,
-            '--far-x': farX,
-            '--far-y': farY,
-            '--note-rotation': rotation,
           } as CSSProperties}
         />
       ))}
